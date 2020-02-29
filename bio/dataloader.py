c09a986855b073402110d3bd0273887788819f33
Ishaan Kumar <ffishaan@gmail.com>
2020-02-29 18:01:32 -0500

    add combined dataloader

--- bio/dataloader.py
+++ bio/dataloader.py
@@ -3,6 +3,7 @@ from torch.utils.data.dataloader import default_collate
 
 from batch import BatchFinetune, BatchMasking, BatchAE, BatchSubstructContext
 
+
 class DataLoaderFinetune(torch.utils.data.DataLoader):
     r"""Data loader which merges data objects from a
     :class:`torch_geometric.data.dataset` to a mini-batch.
@@ -19,8 +20,10 @@ class DataLoaderFinetune(torch.utils.data.DataLoader):
             dataset,
             batch_size,
             shuffle,
-            collate_fn=lambda data_list: BatchFinetune.from_data_list(data_list),
+            collate_fn=lambda data_list: BatchFinetune.from_data_list(
+                data_list),
             **kwargs)
+
 
 class DataLoaderMasking(torch.utils.data.DataLoader):
     r"""Data loader which merges data objects from a
@@ -38,7 +41,8 @@ class DataLoaderMasking(torch.utils.data.DataLoader):
             dataset,
             batch_size,
             shuffle,
-            collate_fn=lambda data_list: BatchMasking.from_data_list(data_list),
+            collate_fn=lambda data_list: BatchMasking.from_data_list(
+                data_list),
             **kwargs)
 
 
@@ -78,8 +82,29 @@ class DataLoaderSubstructContext(torch.utils.data.DataLoader):
             dataset,
             batch_size,
             shuffle,
-            collate_fn=lambda data_list: BatchSubstructContext.from_data_list(data_list),
+            collate_fn=lambda data_list: BatchSubstructContext.from_data_list(
+                data_list),
             **kwargs)
 
 
+class DataLoaderMaskingSubstructContext(torch.utils.data.DataLoader):
+    r"""Data loader which merges data objects from a
+    :class:`torch_geometric.data.dataset` to a mini-batch.
+    Args:
+        dataset (Dataset): The dataset from which to load the data.
+        batch_size (int, optional): How may samples per batch to load.
+            (default: :obj:`1`)
+        shuffle (bool, optional): If set to :obj:`True`, the data will be
+            reshuffled at every epoch (default: :obj:`True`)
+    """
+    @staticmethod
+    def custom_collate_fn(data_list):
+        return BatchMasking.from_data_list(data_list), BatchSubstructContext.from_data_list(data_list)
 
+    def __init__(self, dataset, batch_size=1, shuffle=True, **kwargs):
+        super(DataLoaderMaskingSubstructContext, self).__init__(
+            dataset,
+            batch_size,
+            shuffle,
+            collate_fn=DataLoaderMaskingSubstructContext.custom_collate_fn,
+            **kwargs)
