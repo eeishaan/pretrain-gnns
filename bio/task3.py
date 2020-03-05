0a991223c3464424acc8dcfdafdb9f2ef3fcc6cd
Saaaber <0saber0@gmail.com>
2020-03-05 16:43:26 -0500

    Finetuning and evaluation tasks

--- bio/task3.py
+++ bio/task3.py
@@ -1,15 +1,124 @@
-import torch
+DEBUG = True
+
+import os
 import argparse
 import numpy as np
 from tqdm import tqdm
+from datetime import datetime
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
 from model import GNN
 from util import MaskEdge
 from loader import BioDataset
 from dataloader import DataLoaderMasking
+from pretrain_masking import compute_accuracy
+from splitters import random_split,species_split
+
+if DEBUG is True:
+    import multiprocessing
+    multiprocessing.set_start_method('spawn', True)
 
 
-def main(model, loader, device):
-    pass
+def finetune_linear(model, linear, optimizer, criterion, train_loader, valid_loader,
+                    device, savepath, valid_period=16, valid_steps=16):
+    model.eval()
+    linear.train()
+
+    loss_accum = 0
+    acc_accum = 0
+
+    best_loss = 9999
+
+    valid_iterator = iter(valid_loader)
+
+    for step, batch in enumerate(tqdm(train_loader, desc="Iteration")):
+        linear.train()
+        batch = batch.to(device)
+
+        node_rep = model(batch.x, batch.edge_index, batch.edge_attr)
+
+        ### predict the edge types.
+        masked_edge_index = batch.edge_index[:, batch.masked_edge_idx]
+        edge_rep = node_rep[masked_edge_index[0]] + node_rep[masked_edge_index[1]]
+        pred_edge = linear(edge_rep)
+
+        #converting the binary classification to multiclass classification
+        edge_label = torch.argmax(batch.mask_edge_label, dim = 1)
+
+        acc_edge = compute_accuracy(pred_edge, edge_label)
+        acc_accum += acc_edge
+
+        optimizer.zero_grad()
+
+        loss = criterion(pred_edge, edge_label)
+        loss.backward()
+
+        optimizer.step()
+
+        loss_accum += float(loss.cpu().item())
+
+        #validation iterations
+        if step % valid_period == 0:
+            linear.eval()
+            valid_acc_accum = 0
+            valid_loss_accum = 0
+
+            for _ in range(valid_steps):
+                try:
+                    valid_batch = next(valid_iterator)
+                except StopIteration:
+                    valid_iterator = iter(valid_loader)
+                    valid_batch = next(valid_iterator)
+
+                node_rep = model(valid_batch.x, valid_batch.edge_index, valid_batch.edge_attr)
+
+                ### predict the edge types.
+                masked_edge_index = valid_batch.edge_index[:, valid_batch.masked_edge_idx]
+                edge_rep = node_rep[masked_edge_index[0]] + node_rep[masked_edge_index[1]]
+                pred_edge = linear(edge_rep)
+
+                #converting the binary classification to multiclass classification
+                edge_label = torch.argmax(valid_batch.mask_edge_label, dim = 1)
+
+                valid_acc_edge = compute_accuracy(pred_edge, edge_label)
+                valid_acc_accum += valid_acc_edge
+
+                valid_loss = criterion(pred_edge, edge_label)
+                valid_loss_accum += float(valid_loss.cpu().item())
+
+
+            if best_loss > valid_loss_accum:
+                best_loss = valid_loss_accum
+                torch.save(linear.state_dict(), savepath)
+
+    return loss_accum/(step + 1), acc_accum/(step + 1)
+
+
+def eval_accuracy(model, linear, test_loader, device):
+    model.eval()
+    linear.eval()
+
+    acc_accum = 0
+
+    for step, batch in enumerate(tqdm(test_loader, desc="Iteration")):
+        batch = batch.to(device)
+
+        node_rep = model(batch.x, batch.edge_index, batch.edge_attr)
+
+        ### predict the edge types.
+        masked_edge_index = batch.edge_index[:, batch.masked_edge_idx]
+        edge_rep = node_rep[masked_edge_index[0]] + node_rep[masked_edge_index[1]]
+        pred_edge = linear(edge_rep)
+
+        #converting the binary classification to multiclass classification
+        edge_label = torch.argmax(batch.mask_edge_label, dim = 1)
+
+        acc_edge = compute_accuracy(pred_edge, edge_label)
+        acc_accum += acc_edge
+
+    return acc_accum/(step + 1)
 
 
 if __name__ == "__main__":
@@ -39,7 +148,12 @@ if __name__ == "__main__":
     parser.add_argument('--model_file', type=str, default = '', help='filename to output the model')
     parser.add_argument('--seed', type=int, default=0, help = "Seed for splitting dataset.")
     parser.add_argument('--num_workers', type=int, default = 8, help='number of workers for dataset loading')
-    parser.add_argument('--weights', type=str, default="model_gin/supervised_masking.pth")
+    parser.add_argument('--weights', type=str, default="model_gin/masking.pth",
+                        help='path to weights checkpoint')
+    parser.add_argument('--savedir', type=str, default=None,
+                        help='path where the linear checkpoint is saved')
+    parser.add_argument('--linear_path', type=str, default=None,
+                        help="if specified, evaluates accuracy of models instead of finetuning")
     args = parser.parse_args()
 
     torch.manual_seed(0)
@@ -49,15 +163,102 @@ if __name__ == "__main__":
     if torch.cuda.is_available():
         torch.cuda.manual_seed_all(0)
 
-    #set up dataset
+    # Set up dataset
     root_unsupervised = 'dataset/unsupervised'
-    dataset = BioDataset(root_unsupervised, data_type='unsupervised', transform = MaskEdge(mask_rate = args.mask_rate))
+    dataset = BioDataset(root_unsupervised,
+                         data_type='unsupervised',
+                         transform=MaskEdge(mask_rate=args.mask_rate))
 
     print(dataset)
 
-    loader = DataLoaderMasking(dataset, batch_size=args.batch_size, shuffle=True, num_workers = args.num_workers)
+    # Split the dataset into train-valid-test
+    # To conserve ratios, train-val:test is split 43:7 between species
+    species_list = [int(species) for species in dataset.raw_file_names]
 
-    model = GNN(args.num_layer, args.emb_dim, JK = args.JK, drop_ratio = args.dropout_ratio, gnn_type = args.gnn_type).to(device)
+    test_list = np.random.choice(species_list, size=7, replace=False).tolist()
+    
+    trainval_list = list(set(species_list) - set(test_list))
+    trainval_list.sort()
+
+    trainval_dataset, test_dataset = species_split(dataset,
+                                                   trainval_list,
+                                                   test_list)
+    
+    # Load the model weights
+    model = GNN(args.num_layer,
+                args.emb_dim,
+                JK=args.JK,
+                drop_ratio=args.dropout_ratio,
+                gnn_type=args.gnn_type).to(device)
+
     model.load_state_dict(torch.load(args.weights, map_location=device))
 
-    main(model, loader, device)
+    for p in model.parameters():
+        p.requires_grad=False
+
+    # Get the last layer, either through training or loading a checkpoint
+    linear_pred_edges = torch.nn.Linear(args.emb_dim, 7).to(device)
+
+    if args.linear_path is None:
+        print("Finetuning last layer using: {}".format(args.weights))
+        # Train:val is randomly split 0.85:0.15
+        train_dataset, valid_dataset, _ = random_split(trainval_dataset,
+                                                   seed=args.seed,
+                                                   frac_train=0.85,
+                                                   frac_valid=0.15,
+                                                   frac_test=0)
+
+        train_loader = DataLoaderMasking(train_dataset,
+                                        batch_size=args.batch_size,
+                                        shuffle=True,
+                                        num_workers=args.num_workers)
+
+        valid_loader = DataLoaderMasking(valid_dataset,
+                                        batch_size=args.batch_size,
+                                        shuffle=True,
+                                        num_workers=args.num_workers)
+
+        optimizer_linear_pred_edges = optim.Adam(linear_pred_edges.parameters(),
+                                                lr=args.lr,
+                                                weight_decay=args.decay)
+    
+        criterion = nn.CrossEntropyLoss()
+    
+        if args.savedir is None:
+            savedir = os.path.join("checkpoints/linear", datetime.now().strftime('%m%d_%H%M%S'))
+
+        if os.path.exists(savedir) is False:
+            os.mkdir(savedir)
+    
+        savepath = os.path.join(savedir, "best_masking_loss.pth")
+        
+        loss, acc = finetune_linear(model,
+                                    linear_pred_edges,
+                                    optimizer_linear_pred_edges,
+                                    criterion,
+                                    train_loader,
+                                    valid_loader,
+                                    device,
+                                    savepath)
+
+        print("Final loss:", loss)
+    
+    elif os.path.exists(args.linear_path) is False:
+        print("Incorrect linear checkpoint path")
+        acc = "invalid"
+    
+    else:
+        print("Evaluating model: {} \nUsing last layer: {}".format(args.weights, args.linear_path))
+        test_loader = DataLoaderMasking(test_dataset,
+                                    batch_size=args.batch_size,
+                                    shuffle=False,
+                                    num_workers=args.num_workers)
+
+        linear_pred_edges.load_state_dict(torch.load(args.linear_path, map_location=device))
+        acc = eval_accuracy(model,
+                            linear_pred_edges,
+                            test_loader,
+                            device)
+
+    print("Final acc:", acc)
+    
